db058878feb01bc3c6bdf3f596c3e65e4bd4d042
MichaelJou <864738795@qq.com>
2016-02-25 23:49:51 +0800

    tijiao

--- src/main/java/com/mj/common/interceptors/LoginInterceptor.java
+++ src/main/java/com/mj/common/interceptors/LoginInterceptor.java
@@ -1,13 +1,6 @@
 package com.mj.common.interceptors;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
-import com.mj.common.util.Constant;
-import com.mj.modules.cms.entity.AdminEntity;
 
 
 /**
@@ -19,19 +12,5 @@ public class LoginInterceptor extends HandlerInterceptorAdapter{
 
 
 	
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
-		     HttpSession session =  request.getSession();
-		    if(request.isRequestedSessionIdValid()){
-		    	AdminEntity adminEntity =  (AdminEntity) session.getAttribute(Constant.ADMIN_USER);
-		    	if(adminEntity != null){
-		    		return true;
-		    	}else{
-		    		response.sendRedirect("admin/login.html");
-		    	}
-		    }
-				return false;
-	}
 
 }
