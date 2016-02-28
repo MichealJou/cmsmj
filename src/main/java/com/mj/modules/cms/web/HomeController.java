5c7cacbc93636d857293617a9cb1066f5161898c
MichaelJou <864738795@qq.com>
2016-02-29 00:02:54 +0800

    代码提交

--- src/main/java/com/mj/modules/cms/web/HomeController.java
+++ src/main/java/com/mj/modules/cms/web/HomeController.java
@@ -1,14 +1,16 @@
 package com.mj.modules.cms.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping(value="/")
-	public String index(){
-		return "index";
+	@RequestMapping(value="/index.html")
+	public String index(Model model){
+		  model.addAttribute("user", "你好");
+		return "/WEB-INF/view/index";
 	}
 	
 }
